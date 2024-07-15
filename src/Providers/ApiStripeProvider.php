db3e58c7d2c2997e6e5dab5aca3687073664dc39
Wilber Galindez <wilbergalindezm23@gmail.com>
2024-07-15 08:46:21 -0400

    change version stripe sdk

---
+++ src/Providers/ApiStripeProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wilbere\Stripe\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiStripeProvide extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
