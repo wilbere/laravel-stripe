1514b7099d9f9762631ad01090911e3e074c4af4
Wilber Galindez <wilbergalindezm23@gmail.com>
2024-07-16 10:12:53 -0400

    Test tripe service

--- src/Services/StripeService.php
+++ src/Services/StripeService.php
@@ -14,10 +14,18 @@ class StripeServices
     {
         // Connect to Stripe API
         // You can use the Stripe PHP library
-
         $this->client = new StripeClient($secret_key);
 
+    }
 
+    public function testConnection()
+    {
+        try {
+            $this->client->accounts->retrieve();
 
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
